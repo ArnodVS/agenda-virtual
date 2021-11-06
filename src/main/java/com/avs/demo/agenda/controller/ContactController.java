8be1c163e136da95fd51ce954877c0a5531c44e6
Arnold Valenzuela Sanchez <avs.guy.7@gmail.com>
2021-11-06 00:09:13 -0600

    correccion en editar

--- src/main/java/com/avs/demo/agenda/controller/ContactController.java
+++ src/main/java/com/avs/demo/agenda/controller/ContactController.java
@@ -1,7 +1,6 @@
 package com.avs.demo.agenda.controller;
 
 import com.avs.demo.agenda.model.Contact;
-import com.avs.demo.agenda.model.repo.IContactDao;
 import com.avs.demo.agenda.model.services.IContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,11 +10,14 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
+@SessionAttributes("contact")
 public class ContactController {
 
     @Autowired
@@ -36,13 +38,16 @@ public class ContactController {
     }
 
     @PostMapping("/form")
-    public String save(@Validated Contact contact, BindingResult result, Model model, RedirectAttributes flash){
+    public String save(@Validated Contact contact, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status){
         if(result.hasErrors()){
+            model.addAttribute("title", "Registrar nuevo contacto");
             model.addAttribute("contact", contact);
             return "form";
         }
+        String flashMessages = (contact.getId() != null)? "Contacto editado con éxito!" : "Contacto creado con éxito!";
         contactService.save(contact);
-        flash.addFlashAttribute("successMsg", "El contacto ha sido creado correctamente!");
+        status.setComplete();
+        flash.addFlashAttribute("successMsg", flashMessages);
         return "redirect:/list";
     }
 
@@ -56,9 +61,10 @@ public class ContactController {
 
     @PostMapping("/{id}/edit")
     public String update(@PathVariable Integer id, @Validated Contact contact,
-                         BindingResult result, Model model, RedirectAttributes flash){
+                         BindingResult result, Model model){
         Contact contactDB = contactService.getById(id);
         if(result.hasErrors()){
+            model.addAttribute("title","Actualizar contacto");
             model.addAttribute("contact", contact);
             return "form";
         }
@@ -67,7 +73,6 @@ public class ContactController {
         contactDB.setEmail(contact.getEmail());
         contactDB.setDateBirth(contact.getDateBirth());
         contactService.save(contactDB);
-        flash.addFlashAttribute("successMsg", "El contacto ha sido actualizado correctamente!");
         return "redirect:/list";
     }
 
